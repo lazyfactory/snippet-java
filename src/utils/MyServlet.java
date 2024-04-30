d378e9a6e54238ccb5c768729d7557101cdd11bf
lazyfactory <50354834+lazyfactory@users.noreply.github.com>
2024-04-30 11:12:12 +0900

    Update MyServlet.java

--- src/utils/MyServlet.java
+++ src/utils/MyServlet.java
@@ -44,6 +44,38 @@ public class MyServlet extends HttpServlet {
 		res = request(url, req.getMethod(), res);
 	}
 
+	public static String getBody(HttpServletRequest request) throws IOException {
+	 
+	        String body = null;
+	        StringBuilder stringBuilder = new StringBuilder();
+	        BufferedReader bufferedReader = null;
+	 
+	        try {
+	            InputStream inputStream = request.getInputStream();
+	            if (inputStream != null) {
+	                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+	                char[] charBuffer = new char[128];
+	                int bytesRead = -1;
+	                while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+	                    stringBuilder.append(charBuffer, 0, bytesRead);
+	                }
+	            }
+	        } catch (IOException ex) {
+	            throw ex;
+	        } finally {
+	            if (bufferedReader != null) {
+	                try {
+	                    bufferedReader.close();
+	                } catch (IOException ex) {
+	                    throw ex;
+	                }
+	            }
+	        }
+	 
+	        body = stringBuilder.toString();
+	        return body;
+	}
+
 	HttpServletResponse request(String reqUrl, String method, HttpServletResponse res) throws IOException {
 		System.out.println(reqUrl + " requested....");
 		URL url = new URL(reqUrl);
