c39eccec8b9ff0cf475138ec50c14f699c31b74d
lazyfactory <50354834+lazyfactory@users.noreply.github.com>
2024-04-30 12:11:11 +0900

    Update JsonParse.java

--- src/utils/JsonParse.java
+++ src/utils/JsonParse.java
@@ -3,11 +3,18 @@ package utils;
 import com.google.gson.*;
 
 public class JsonParse {
-  public static JsonObject parse(String str) {
-    JsonParser parser = new JsonParser();
-    JsonElement element = parser.parse(str);
-    return element.getAsJsonObject();
- }
+	static void makeJson() {
+		JsonObject jsonObject = new JsonObject();        
+		jsonObject.addProperty("name", "anna");        
+		jsonObject.addProperty("id", 1);         // JsonObject를 Json 문자열로 변환        
+		String jsonStr = gson.toJson(jsonObject);
+	}
+	
+	  public static JsonObject parse(String str) {
+	    JsonParser parser = new JsonParser();
+	    JsonElement element = parser.parse(str);
+	    return element.getAsJsonObject();
+	 }
 
 	static String getJson(JsonObject respObj) {
 		GsonBuilder builder = new GsonBuilder();
